9b921b11ab50d198032dfd5b0db7a70ff6700e9f
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-10-24 08:34:00 +0300

    fix layout bug

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ export const App: React.FC = () => {
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
-
       <UserTodos userId={Number(userId)} />
     </div>
   );
